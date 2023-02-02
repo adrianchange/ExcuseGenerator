e5970db47d06551579c883f19675b0f200d74564
adrianchange <adrianchange@gmail.com>
2023-02-02 21:16:07 +0000

    Finished the project

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  console.log("");
+};
+
+let generateExcuse = () => {
+  let pronun = ["The", "A"];
+  let subject = ["dog", "racoon", "squirl", "mouse"];
+  let verb = ["stole my", "destroy my", "threw my"];
+  let atribute = ["homework", "dinner", "hat"];
+  let complement = ["in the highway.", "in the parc."];
+
+  let pronIndex = Math.floor(Math.random() * pronun.length);
+  let subIndex = Math.floor(Math.random() * subject.length);
+  let verbIndex = Math.floor(Math.random() * verb.length);
+  let atribIndex = Math.floor(Math.random() * atribute.length);
+  let compleIndex = Math.floor(Math.random() * complement.length);
+
+  return (
+    pronun[pronIndex] +
+    " " +
+    subject[subIndex] +
+    " " +
+    verb[verbIndex] +
+    " " +
+    atribute[atribIndex] +
+    " " +
+    complement[compleIndex]
+  );
 };
